3cb4296ed5feeebf395ec5a81514eb75efa92acb
Юрий Бреславец (d1gitable) <yuri.breslavets@d1gitable.ru>
2015-09-27 17:45:55 +0300

    edit template

--- gulpfile.js
+++ gulpfile.js
@@ -8,23 +8,15 @@ var concat  = require('gulp-concat');
 var rename = require('gulp-rename');
 var sass    = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
-var changed = require('gulp-changed');
-var imagemin = require('gulp-imagemin');
 var uglify = require('gulp-uglify');
 var autoprefix = require('gulp-autoprefixer');
-
-var minifyHTML = require('gulp-minify-html');
 var jshint = require('gulp-jshint');
-
 var stripDebug = require('gulp-strip-debug');
-
 var clean = require('gulp-clean');
-var minifyCSS = require('gulp-minify-css');
-
-
-
-
+var minifycss = require('gulp-minify-css');
+var notify = require('gulp-notify');
 var jade = require('gulp-jade');
+var plumber = require('gulp-plumber');
 
 var version     = pkg.version;
 var name        = pkg.name;
@@ -41,18 +33,27 @@ var source_sass     = './src/sass/styles.scss';
 //custom destination
 var destination         = '../catalog/view/theme/' + name + '/assets/';
 var destination_css     = '../catalog/view/theme/' + name + '/assets/css/';
-var destination_js      = '../catalog/view/theme/' + name + '/assets/scripts/';
+var destination_js      = '../catalog/view/theme/' + name + '/assets/js/';
 var destination_fonts   = '../catalog/view/theme/' + name + '/assets/fonts/';
 var destination_image   = '../catalog/view/theme/' + name + '/assets/css/img';
 
 // tasks copy font-awesome 
-gulp.task('copyfont', function() {
-    gulp.src('./bower_components/font-awesome/fonts/*')
-        .pipe(gulp.dest(fontsTarget+'font-awesome'));
+gulp.task('copyfontawesome', function() {
+    return gulp.src('./bower_components/font-awesome/fonts/*')
+        .pipe(gulp.dest(destination_fonts+'font-awesome'));
+});
+// tasks copy font-glyphicon 
+gulp.task('copyfontglyphicon', function() {
+    return gulp.src('./bower_components/bootstrap-sass/assets/fonts/**/*')
+    .pipe(gulp.dest(destination_fonts))
+    .pipe(notify({
+            title: 'copyfontglyphicon',
+            message: 'copy - DONE!'
+    }));
 });
 //clean task
 gulp.task('clean', function() {
-   return gulp.src(destination,{
+    return gulp.src(destination,{
             read:false
         })
         .pipe(clean({force: true}));
@@ -61,12 +62,12 @@ gulp.task('clean', function() {
 //sass task
 gulp.task('sass', function() {
     gulp.src(source_sass)
+        .pipe(plumber())
         .pipe(sourcemaps.init())
-
         .pipe(sass({
            errLogToConsole: true
-        }))
-        .pipe(autoprefix(
+        }).on('error', sass.logError))
+       /* .pipe(autoprefix(
             'Android >= ' + browsers.android,
             'Chrome >= ' + browsers.chrome,
             'Firefox >= ' + browsers.firefox,
@@ -74,74 +75,87 @@ gulp.task('sass', function() {
             'iOS >= ' + browsers.ios,
             'Opera >= ' + browsers.opera,
             'Safari >= ' + browsers.safari
-        ))
+        ))*/
         .pipe(rename(name+'.css'))
         .pipe(gulp.dest(destination_css))
-        .pipe(minifyCSS())
+        .pipe(minifycss())
         .pipe(rename(name+'.min.css'))
         .pipe(sourcemaps.write())
+        .pipe(notify({
+            title: 'sass',
+            message: 'DONE!'
+        }))
         .pipe(gulp.dest(destination_css));
 });
-
-// JS hint task
+// JS concat, strip debugging and minify
+var sourcesjs  = [
+    './bower_components/modernizr/modernizr.js',
+    './bower_components/jquery/dist/jquery.js',
+    './bower_components/bootstrap-sass/assets/javascripts/bootstrap/transition.js',
+    //'./bower_components/bootstrap-sass/assets/javascripts/bootstrap/alert.js',
+    //'./bower_components/bootstrap-sass/assets/javascripts/bootstrap/button.js',
+    //'./bower_components/bootstrap-sass/assets/javascripts/bootstrap/carousel.js',
+    './bower_components/bootstrap-sass/assets/javascripts/bootstrap/collapse.js',
+    './bower_components/bootstrap-sass/assets/javascripts/bootstrap/dropdown.js',
+    './bower_components/bootstrap-sass/assets/javascripts/bootstrap/modal.js',
+    //'./bower_components/bootstrap-sass/assets/javascripts/bootstrap/tooltip.js',
+    //'./bower_components/bootstrap-sass/assets/javascripts/bootstrap/popover.js',
+    //'./bower_components/bootstrap-sass/assets/javascripts/bootstrap/scrollspy.js',
+    './bower_components/bootstrap-sass/assets/javascripts/bootstrap/tab.js',
+    //'./bower_components/bootstrap-sass/assets/javascripts/bootstrap/affix.js', 
+    'src/scripts/_visual.js',
+    'src/scripts/_action.js',
+    'src/scripts/_rival.js',
+    'src/scripts/_common.js'
+    
+];
+var custom_sourcesjs = [
+    'src/scripts/_visual.js',
+    'src/scripts/_action.js',
+    'src/scripts/_rival.js',
+    'src/scripts/_common.js'
+];
+gulp.task('scripts', function() {
+    return gulp.src(sourcesjs)
+        .pipe(plumber())
+        .pipe(concat(name+'.js'))
+        .pipe(gulp.dest(destination_js))
+        .pipe(notify({
+            title: 'scripts',
+            message: 'concat - DONE!'
+        }))
+        .pipe(uglify())
+        .pipe(rename(name+'.min.js'))
+        .pipe(gulp.dest(destination_js))
+        .pipe(notify({
+            title: 'scripts',
+            message: 'uglify - DONE!'
+        }));
+});
+// JS hint task ???
 gulp.task('jshint', function() {
-    gulp.src('./src/scripts/*.js')
+    return gulp.src(custom_sourcesjs)
+        .pipe(plumber())
+        .pipe(stripDebug())
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
-// minify new images
-gulp.task('imagemin', function() {
-    gulp.src(imgSrc)
-        .pipe(changed(imgDst))
-        .pipe(imagemin())
-        .pipe(gulp.dest(imgDst));
-});
-// minify new or changed HTML pages
-gulp.task('htmlpage', function() {
-    gulp.src(htmlSrc)
-        .pipe(changed(htmlDst))
-        .pipe(minifyHTML())
-        .pipe(gulp.dest(htmlDst));
-});
-// JS concat, strip debugging and minify
-gulp.task('scripts', function() {
-    gulp.src(sourcesjs)
-        .pipe(concat('script.js'))
-        .pipe(stripDebug())
-        .pipe(uglify())
-
-    .pipe(gulp.dest(pathjstarget));
-});
-
-
-
-
-
 
 //jade task
-gulp.task('jade', function() {
-    gulp.src([srcjade])
-        .pipe(jade())
-        .pipe(gulp.dest(jadetarget))
+//gulp.task('jade', function() {
+//    gulp.src([srcjade])
+//        .pipe(jade())
+//        .pipe(gulp.dest(jadetarget))
+//});
 
+// default gulp task
+gulp.task('default', function() {
+   gulp.run('copyfontawesome','sass', 'scripts'); 
 });
 // default gulp task
-gulp.task('default', ['imagemin', 'scripts', 'sass', 'jade'], function() {
-    // watch for JS changes
-    gulp.watch('./src/scripts/*.js', function() {
-        gulp.run('jshint', 'scripts');
-    });
-
-    
-    gulp.watch('./src/sass/{,*/}*.{scss,sass}', function() {
-        gulp.run('sass');
-    });
-    gulp.watch('./src/images/**/*', function() {
-        gulp.run('imagemin');
-    });
-
-    gulp.task('./src/jade/**/*', function() {
-        gulp.run('jade');
-    });
-
+gulp.task('watch', function() {
+    gulp.watch('./src/scripts/*.js', ['jshint','scripts']);
+    gulp.watch('./src/sass/**/*.scss', ['sass']);
+    gulp.watch('./src/jade/**/*', ['jade']);
 });
+
---
+++ src/scripts/_common.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	console.log('ready document');
+	visual.init();
+});
+$(window).on('resize', function(){
+	visual.resize();
+});
+$(window).on('load', function(){
+  	visual.preloader();
+  	
+});
+$(window).on('scroll', function(){
+    visual.scrolling();
+});
